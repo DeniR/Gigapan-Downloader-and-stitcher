ca47385ee758b2e6bde3fa9c731e8b4c3e1367cd
DeniR <denis@raguzov.ru>
2014-03-13 06:05:22 -0700

    Fixed Issue #1

--- gigapanDownloader.py
+++ gigapanDownloader.py
@@ -73,5 +73,5 @@ for j in xrange(ht):
 	        fout.write(h.read())
 	        fout.close()
 print "Stitching... "
-subprocess.call('"'+imagemagick+'" -depth 8 -geometry 256x256+0+0 -mode concatenate -tile '+str(wt)+'x '+str(photo_id)+'\\*.jpg '+str(photo_id)+'-giga.'+outputformat) 
+subprocess.call('"'+imagemagick+'" -depth 8 -geometry 256x256+0+0 -mode concatenate -tile '+str(wt)+'x '+str(photo_id)+'\*.jpg '+str(photo_id)+'-giga.'+outputformat, shell=True)
 print "OK"
